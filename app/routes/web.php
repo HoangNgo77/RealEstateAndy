22bc28f53c08f4b0165e43eba6867148151238c7
Duy Nguyen <duy.nguyen@topdev.vn>
2025-09-02 13:33:02 +0700

    add assest, route

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'pages.home');
+Route::view('about', 'pages.about');
+Route::view('properties', 'pages.properties');
+Route::view('property-details', 'pages.property-details');
+Route::view('blog', 'pages.blog');
+Route::view('blog-detail', 'pages.blog-detail');
+Route::view('contact', 'pages.contact');
