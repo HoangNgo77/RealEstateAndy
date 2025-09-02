0aa01432772e026cb4a3ab5b70e2333c35a4c7f8
Duy Nguyen <duy.nguyen@topdev.vn>
2025-09-02 13:36:38 +0700

    add pages

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.home');
 Route::view('about', 'pages.about');
 Route::view('properties', 'pages.properties');
-Route::view('property-details', 'pages.property-details');
+Route::view('property-detail', 'pages.property-detail');
 Route::view('blog', 'pages.blog');
 Route::view('blog-detail', 'pages.blog-detail');
 Route::view('contact', 'pages.contact');
