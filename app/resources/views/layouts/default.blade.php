08234f2840e218d6a304da4304f53db913e15c95
Duy Nguyen <nguyenduy4994@gmail.com>
2025-10-08 20:08:59 +0700

    update layout

--- app/resources/views/layouts/default.blade.php
+++ app/resources/views/layouts/default.blade.php
@@ -84,13 +84,10 @@
     <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery-ui.min.js') }}"></script>
     <script src="{{ asset('assets/js/lenis.min.js') }}"></script>
-
     <script src="{{ asset('assets/js/360.min.js') }}"></script>
     <script src="{{ asset('assets/js/panolens.min.js') }}"></script>
-
     <script src="{{ asset('assets/js/wow.min.js') }}"></script>
     <script src="{{ asset('assets/js/nice-select.min.js') }}"></script>
-
     <script src="{{ asset('assets/js/main.js') }}"></script>
   </body>
 
