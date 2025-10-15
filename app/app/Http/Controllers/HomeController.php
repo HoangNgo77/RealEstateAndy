02fdfc0384d770e98c71ac21d4bc7e5767c4f3fd
Duy Nguyen <nguyenduy4994@gmail.com>
2025-10-15 14:32:49 +0700

    feat: display best properties on the homepage with dynamic data

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.home');
+        $bestProperties = $this->getBestProperties();
+
+        return view('pages.home', compact('bestProperties'));
+    }
+
+    private function getBestProperties()
+    {
+        return Property::query()
+            ->orderBy('created_at', 'desc')
+            ->take(20)
+            ->get();
     }
 }
