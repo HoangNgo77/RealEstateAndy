88adec137a06bab4080cdad7e9459840cd2747d6
Tan Nguyen <tannp27@gmail.com>
2025-12-03 19:42:47 +0700

    feat: add GeneralSettingSeeder for initializing site settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,14 @@ class DatabaseSeeder extends Seeder
         $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
 
         $adminRole = Role::findByName('admin');
         $user->assignRole($adminRole);
 
         $this->call([
+            GeneralSettingSeeder::class,
             AmenitySeeder::class,
             PropertySeeder::class,
             PostSeeder::class,
